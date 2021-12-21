45abdfe8173426c9ee97d58b34c0c423639dc0ab
Faber Ordoñez <faberordonez1@gmail.com>
2021-12-20 22:53:44 -0500

    ng style

--- src/app/components/pelicula/pelicula.component.ts
+++ src/app/components/pelicula/pelicula.component.ts
@@ -13,9 +13,9 @@ export class PeliculaComponent implements OnInit {
   constructor() { 
     this.titulo = "Componente pelicula"
     this.peliculas=[
-      {title:"Spider Man 4",imagen:"https://i2.wp.com/codigoespagueti.com/wp-content/uploads/2021/10/Spider-Man-4-Fanat.jpg?fit=1280%2C720&quality=80&ssl=1"},
-      {title:"Los vengadores",imagen:"https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/vengadores-endgame-poster-1556056595.jpg?crop=1.00xw:0.352xh;0,0.131xh&resize=1200:*"},
-      {title:"Batman vs Superman",imagen:"https://www.cinemascomics.com/wp-content/uploads/2020/06/snyder-cut-batman-vs-superman.jpg"}
+      {year:2019,title:"Spider Man 4",imagen:"https://i2.wp.com/codigoespagueti.com/wp-content/uploads/2021/10/Spider-Man-4-Fanat.jpg?fit=1280%2C720&quality=80&ssl=1"},
+      {year:2017,title:"Los vengadores",imagen:"https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/vengadores-endgame-poster-1556056595.jpg?crop=1.00xw:0.352xh;0,0.131xh&resize=1200:*"},
+      {year:2015,title:"Batman vs Superman",imagen:"https://www.cinemascomics.com/wp-content/uploads/2020/06/snyder-cut-batman-vs-superman.jpg"}
     ];
   }
 
